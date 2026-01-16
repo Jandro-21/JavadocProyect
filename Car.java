a85908b5890aef92afdee62fba5cdea730f0e0bf
AlejandroDurillo <adurvar2106@g.educaand.es>
2026-01-16 20:15:01 +0100

    Commit con doc

--- Car.java
+++ Car.java
@@ -1,9 +1,32 @@
 public class Car {
+    /**
+     * Marca del automóvil (por ejemplo, Toyota)
+     */
     private String make;
+
+    /**
+     * Modelo del automóvil (por ejemplo, Corolla)
+     */
     private String model;
+
+    /**
+     * Año de fabricación del automovil
+     */
     private int year;
+
+    /**
+     * Precio del automovil
+     */
     private double price;
 
+    /**
+     * Crea un nuevo public de Car 
+     *
+     * @param make  
+     * @param model 
+     * @param year  
+     * @param price 
+     */
     public Car(String make, String model, int year, double price) {
         this.make = make;
         this.model = model;
@@ -11,54 +34,48 @@ public class Car {
         this.price = price;
     }
 
+    /**
+     * Obtiene la marca del automovil
+     *
+     * @return Marca del automovil
+     */
     public String getMake() {
         return make;
     }
 
+    /**
+     * Establece la marca del automovil
+     *
+     * @param make nueva marca
+     */
     public void setMake(String make) {
         this.make = make;
     }
 
+    /**
+     * Obtiene el modelo del automovil
+     *
+     * @return Modelo del automovil
+     */
     public String getModel() {
         return model;
     }
 
+    /**
+     * Establece el modelo del automovil
+     *
+     * @param model Nuevo modelo
+     */
     public void setModel(String model) {
         this.model = model;
     }
 
+    /**
+     * Obtiene el año de fabricación del automovil
+     *
+     * @return Año de fabricacion
+     */
     public int getYear() {
         return year;
     }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String carDescription() {
-        return "the car is a " + year + " " + make + " " + model + " Priced at €" + price;
-    }
-
-    @Override
-    public String toString() {
-        return carDescription();
-    }
-
-    public static void main(String[] args) {
-        Car car = new Car("Toyota", "Corolla", 2025, 27800);
-        System.out.println(car);
-    
-    }
-
-    
-   
-    
 }
